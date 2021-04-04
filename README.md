79e87323f3163fb00627b632c39e203b1b3c660c
Pedro <pedrogas_g@hotmail.com>
2021-04-04 03:07:13 +0200

    Changing to structs

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ source /microros_ws/install/local_setup.bash
 echo "CONFIG_ESP_WIFI_SSID=$SSID" >> /microros_ws/firmware/freertos_apps/microros_esp32_extensions/sdkconfig.defaults
 echo "CONFIG_ESP_WIFI_PASSWORD=$WIFI_PASSWORD" >> /microros_ws/firmware/freertos_apps/microros_esp32_extensions/sdkconfig.defaults
 
-ros2 run micro_ros_setup configure_firmware.sh two_wheels -t udp -i ${MYIP} -p 8888
+ros2 run micro_ros_setup configure_firmware.sh servo_motor -t udp -i ${MYIP} -p 8888
 
 ros2 run micro_ros_setup build_firmware.sh
 
---
+++ apps/servo_motor/include/servo.h
@@ -0,0 +1,41 @@
+# pragma once
+
+#include <rcl/rcl.h>
+
+#define SERVO_MIN_PULSEWIDTH 1000 //Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH 2000 //Maximum pulse width in microsecond
+#define SERVO_MAX_DEGREE 90 //Maximum angle in degree upto which servo can rotate
+
+
+struct servo_impl_t;
+
+typedef struct servo_t
+{
+    struct servo_impl_t * impl;
+    
+}servo_t;
+
+typedef struct servo_options_t
+{   int32_t gpio; // PINOUT in the ESP32 board    
+    int32_t max_pulse; //Minimum pulse width in microsecond
+    int32_t min_pulse; //Maximum pulse width in microsecond
+    int32_t max_degree; //Maximum angle in degree upto which servo can rotate
+    int32_t frequency; //50Hz, i.e. for every servo motor time period should be 20ms
+}servo_options_t;
+
+
+rcl_ret_t
+servo_init(
+    servo_t * servo,
+    const servo_options_t * options,
+    const rcl_allocator_t * allocator);
+
+int32_t
+servo_per_degree(
+    servo_t * servo,
+    int32_t degree_of_rotation);
+
+void
+servo_control(
+    servo_t * servo,
+    int32_t degree);
