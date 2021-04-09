332ef0860e961877afd7215e00d73a88d3d4ce06
Pedro <pedrogas_g@hotmail.com>
2021-04-09 11:53:43 +0200

    build script and libs in folder

---
+++ apps/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+APP_DIRECTORY=$1
+
+if [ -d "$APP_DIRECTORY" ] 
+then
+    cd $APP_DIRECTORY
+    
+    if [ ! -f "sdkconfig" ]; then
+        echo "CONFIG_ESP_WIFI_SSID=\"$SSID\"" >> sdkconfig.defaults
+
+        echo "CONFIG_ESP_WIFI_PASSWORD=\"$WIFI_PASSWORD\"" >> sdkconfig.defaults
+
+        echo "CONFIG_MICRO_ROS_AGENT_IP=\"$MYIP\"" >> sdkconfig.defaults
+
+        echo "CONFIG_MICRO_ROS_AGENT_PORT=\"8888\"" >> sdkconfig.defaults
+    fi
+
+    if [ -f "colcon.meta" ]; then
+        cp colcon.meta ../../colcon.meta
+    fi
+
+    idf.py build
+
+    read -n 1 -s -r -p 'Flash the device y/n: ' flash; echo
+
+    if [ "$flash" = "y" ]; then
+
+        read -n 1 -s -r -p 'Monitor after flash y/n: ' monitor; echo
+
+        if [ "$monitor" = "y" ]; then
+            idf.py flash monitor
+        else
+            idf.py flash
+        fi
+    fi
+
+
+else
+    echo "Error: Directory $APP_DIRECTORY does not exists in applications folder."
+fi
+
--- launch.sh
+++ launch.sh
@@ -2,4 +2,4 @@
 
 docker build -t esp-microros .
 
-docker run -it --rm --env WIFI_PASSWORD --env SSID --env MYIP --volume="$PWD"/apps:/microros_ws/component/apps:rw --net=host -v /dev:/dev --privileged esp-microros /bin/bash
+docker run -it --rm --env WIFI_PASSWORD --env SSID --env MYIP --volume="$PWD"/apps:/microros_ws/component/apps:rw --net=host -v /dev:/dev -w="/microros_ws/component/apps" --privileged esp-microros /bin/bash
